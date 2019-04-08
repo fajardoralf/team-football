d709443d907f571ad12a3a9649cf3eec1ee36c77
“NightSkyHigh” <Thomas_95_@hotmail.com>
2019-04-08 13:54:40 +0200

    Added models

---
+++ football-api/football-api/src/main/java/com/norofff/team1/footballapi/model/GoalType.java
@@ -0,0 +1,12 @@
+package com.norofff.team1.footballapi.model;
+
+import lombok.Data;
+
+@Data
+public class GoalType {
+    private int goal_type_id;
+    private String type;
+
+    public GoalType(){}
+
+}
