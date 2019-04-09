2471c440f4025849a90e493a80c20bb5b5402b16
fajardoralf <fajardoralf@gmail.com>
2019-04-09 21:46:17 +0200

    fixed spring bug

---
+++ src/main/java/com/norofff/team1/footballapi/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.norofff.team1.footballapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "Hello Welcome to our database";
+    }
+}
