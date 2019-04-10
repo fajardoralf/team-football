bd56827abe0f5cec18fdb28c0e9f70a9e60a13cf
“NightSkyHigh” <Thomas_95_@hotmail.com>
2019-04-10 12:55:18 +0200

    added MatchPosition service and methods

--- src/main/java/com/norofff/team1/footballapi/repository/MatchPosition_Repository.java
+++ src/main/java/com/norofff/team1/footballapi/repository/MatchPosition_Repository.java
@@ -3,8 +3,14 @@ package com.norofff.team1.footballapi.repository;
 import com.norofff.team1.footballapi.model.MatchPosition;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
-public interface MatchPosition_Repository extends JpaRepository<MatchPosition, Integer> {
+@Transactional
+public interface MatchPosition_Repository extends JpaRepository<MatchPosition, MatchPositionId> {
     List<MatchPosition> findAll();
+
+    MatchPosition getOne(MatchPositionId id);
+    //MatchPosition save(MatchPosition matachPosition);
+    //void deleteById(int id);
 }
--- src/main/java/com/norofff/team1/footballapi/service/MatchPosition_Service.java
+++ src/main/java/com/norofff/team1/footballapi/service/MatchPosition_Service.java
@@ -1,13 +1,15 @@
 package com.norofff.team1.footballapi.service;
 
 import com.norofff.team1.footballapi.model.MatchPosition;
+import com.norofff.team1.footballapi.repository.MatchPositionId;
 
 import java.util.List;
 
 public interface MatchPosition_Service {
     List<MatchPosition> findAll();
 
-    /*MatchPosition create(MatchPosition matchPosition);
-    MatchPosition update(int id, MatchPosition matchPosition);
-    void delete(int id);*/
+    MatchPosition getOne(MatchPositionId id);
+    //MatchPosition create(MatchPosition matchPosition);
+   // MatchPosition update(MatchPositionId id, MatchPosition matchPosition);
+    //void delete(int id);
 }
