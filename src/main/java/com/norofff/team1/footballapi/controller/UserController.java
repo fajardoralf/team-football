02d09a938d9f4ceb285bee04eb430f8d90a41a42
fajardoralf <fajardoralf@gmail.com>
2019-04-10 13:24:10 +0200

    cors prob

---
+++ src/main/java/com/norofff/team1/footballapi/controller/UserController.java
@@ -0,0 +1,66 @@
+package com.norofff.team1.footballapi.controller;
+
+import com.norofff.team1.footballapi.model.Users;
+import com.norofff.team1.footballapi.service.User_Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    private final User_Service user_service;
+
+    @Autowired
+    public UserController(User_Service user_service){
+        this.user_service = user_service;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<Users>> findAll() {
+        try {
+            List<Users> character_classes = user_service.findAll();
+            return new ResponseEntity<>(character_classes, HttpStatus.FOUND);
+        } catch (DataAccessException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("users/{id}")
+    public ResponseEntity<Users> getOne(@PathVariable int id){
+        try{
+            Users team = user_service.getOne(id);
+            return new ResponseEntity<>(team, HttpStatus.FOUND);
+        }catch(DataAccessException e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<Users> create(@RequestBody Users users){
+        try{
+            user_service.create(users);
+            return new ResponseEntity<>(users, HttpStatus.ACCEPTED);
+        }catch (DataAccessException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping(value = "/users/{id}")
+    public ResponseEntity<Users> update(@PathVariable int id, @RequestBody Users users){
+        try{
+            user_service.update(id, users);
+            return new ResponseEntity<>(users, HttpStatus.ACCEPTED);
+        }catch (DataAccessException e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping(value = "/users/{id}")
+    public void delete(@PathVariable int id){
+        user_service.delete(id);
+    }
+}
