75cd47aceaf2ebf2e0486340961da5d0396b4f30
“NightSkyHigh” <Thomas_95_@hotmail.com>
2019-04-12 10:58:07 +0200

    removed association_id from match model

--- src/main/java/com/norofff/team1/footballapi/model/Team.java
+++ src/main/java/com/norofff/team1/footballapi/model/Team.java
@@ -14,24 +14,21 @@ public class Team {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int team_id;
     private int owner_id;
-    private int association_id;
     private int coach_id;
     private int location_id;
     private String team_name;
     public Team(){}
 
-    public Team(int team_id, int owner_id, int association_id, int coach_id, int location_id, String team_name) {
+    public Team(int team_id, int owner_id, int coach_id, int location_id, String team_name) {
         this.team_id = team_id;
         this.owner_id = owner_id;
-        this.association_id = association_id;
         this.coach_id = coach_id;
         this.location_id = location_id;
         this.team_name = team_name;
     }
     //Postmann constructor
-    public Team(int owner_id, int association_id, int coach_id, int location_id, String team_name) {
+    public Team(int owner_id, int coach_id, int location_id, String team_name) {
         this.owner_id = owner_id;
-        this.association_id = association_id;
         this.coach_id = coach_id;
         this.location_id = location_id;
         this.team_name = team_name;
