ddd50b8ff95588be604fe292b43160fefe1058eb
“NightSkyHigh” <Thomas_95_@hotmail.com>
2019-04-12 10:49:06 +0200

    added team_name variable in team model

--- src/main/java/com/norofff/team1/footballapi/model/Team.java
+++ src/main/java/com/norofff/team1/footballapi/model/Team.java
@@ -17,21 +17,23 @@ public class Team {
     private int association_id;
     private int coach_id;
     private int location_id;
-
+    private String team_name;
     public Team(){}
 
-    public Team(int team_id, int owner_id, int association_id, int coach_id, int location_id) {
+    public Team(int team_id, int owner_id, int association_id, int coach_id, int location_id, String team_name) {
         this.team_id = team_id;
         this.owner_id = owner_id;
         this.association_id = association_id;
         this.coach_id = coach_id;
         this.location_id = location_id;
+        this.team_name = team_name;
     }
     //Postmann constructor
-    public Team(int owner_id, int association_id, int coach_id, int location_id) {
+    public Team(int owner_id, int association_id, int coach_id, int location_id, String team_name) {
         this.owner_id = owner_id;
         this.association_id = association_id;
         this.coach_id = coach_id;
         this.location_id = location_id;
+        this.team_name = team_name;
     }
 }
