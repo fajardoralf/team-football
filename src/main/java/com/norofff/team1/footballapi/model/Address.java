077b097e9d6337ef37316c654dde40fcfcaf2fe8
Trudelutten <t.b.hjelmeland@gmail.com>
2019-04-12 09:51:28 +0200

    updated teamcontroller

--- src/main/java/com/norofff/team1/footballapi/model/Address.java
+++ src/main/java/com/norofff/team1/footballapi/model/Address.java
@@ -14,41 +14,18 @@ public class Address {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int address_id;
     private String address_line_1;
-    private String address_line_2;
     private String postal_code;
     private String city;
     private String country;
-    private String address_line_3;
 
     public Address(){
 
-    }
-
-    public Address(int address_id, String address_line_1, String address_line_2, String postal_code, String city, String country, String address_line_3) {
-        this.address_id = address_id;
-        this.address_line_1 = address_line_1;
-        this.address_line_2 = address_line_2;
-        this.postal_code = postal_code;
-        this.city = city;
-        this.country = country;
-        this.address_line_3 = address_line_3;
     }
 
     //NB: constructor for data population
     public Address(String address_line_1, String postal_code, String city, String country) {
         this.address_id = address_id;
         this.address_line_1 = address_line_1;
-        this.address_line_2 = address_line_2;
-        this.postal_code = postal_code;
-        this.city = city;
-        this.country = country;
-        this.address_line_3 = address_line_3;
-    }
-
-    public Address(int address_id, String address_line_1, String address_line_2, String postal_code, String city, String country) {
-        this.address_id = address_id;
-        this.address_line_1 = address_line_1;
-        this.address_line_2 = address_line_2;
         this.postal_code = postal_code;
         this.city = city;
         this.country = country;
