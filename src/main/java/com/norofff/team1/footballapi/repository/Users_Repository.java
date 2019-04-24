eb0d4ed5ec06e9b1c4dd3468d1a9362fcae454eb
“NightSkyHigh” <Thomas_95_@hotmail.com>
2019-04-24 17:21:17 +0200

    spring security login with encoded password

--- src/main/java/com/norofff/team1/footballapi/repository/Users_Repository.java
+++ src/main/java/com/norofff/team1/footballapi/repository/Users_Repository.java
@@ -2,6 +2,7 @@ package com.norofff.team1.footballapi.repository;
 
 import com.norofff.team1.footballapi.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,8 +12,11 @@ import java.util.Optional;
 public interface Users_Repository extends JpaRepository<Users, Integer> {
     List<Users> findAll();
 
-    Optional<Users> findByUsername(String username);
     Users getOne(int id);
     Users save(Users users);
     void deleteById(int id);
+
+    @Query(value = "select * from users where username=?", nativeQuery = true)
+    Optional<Users> findByUsername(String username);
+
 }
