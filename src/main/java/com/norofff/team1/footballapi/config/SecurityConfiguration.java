180a6cf26ed312acbb1a9a1281ba5458af02d832
“NightSkyHigh” <Thomas_95_@hotmail.com>
2019-04-25 12:02:02 +0200

    login endpoint

--- src/main/java/com/norofff/team1/footballapi/config/SecurityConfiguration.java
+++ src/main/java/com/norofff/team1/footballapi/config/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -16,12 +17,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @EnableJpaRepositories(basePackageClasses = Users_Repository.class)
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
     @Autowired
     private MyUserDetailsService userDetailsService;
@@ -34,18 +37,47 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity httpSecurity) throws Exception {
 
-        httpSecurity.csrf().disable();
-        httpSecurity
+                /*httpSecurity.csrf().disable()
                 .authorizeRequests()
-                //.antMatchers("/users").authenticated()
+                .antMatchers(HttpMethod.GET,"/index*","/static/**", "/*.json", "/*.ico")
+                .permitAll()
+                .anyRequest().authenticated()
+                        .and()
+                        .formLogin().loginPage("/index.html")
+                        .loginProcessingUrl("/perform_login")
+                        .defaultSuccessUrl("/homepage.html",true)
+                        .failureUrl("/index.html?error=true");*/
+
+        httpSecurity.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/users").authenticated()
                 .anyRequest()
                 .permitAll()
                 .and().formLogin()
-                //.loginPage("/loginpage")
+                //.loginPage("http:/localhost:3000")
                 .permitAll();
 
-
     }
+
+   /*@Override
+   protected void configure(HttpSecurity http) throws Exception {
+       http
+               .authorizeRequests()
+               .antMatchers("/bower_components/**", "/*.js",
+                       "/*.jsx", "/main.css").permitAll()
+               .anyRequest().authenticated()
+               .and()
+               .formLogin()
+               .defaultSuccessUrl("/", true)
+               .permitAll()
+               .and()
+               .httpBasic()
+               .and()
+               .csrf().disable()
+               .logout()
+               .logoutSuccessUrl("/");
+   }*/
+
 
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
