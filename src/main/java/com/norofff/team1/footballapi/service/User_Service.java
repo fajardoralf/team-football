06ea32a0e6ebac9f7b56ef5b33f7d99d2f2320f5
“NightSkyHigh” <Thomas_95_@hotmail.com>
2019-04-25 12:11:59 +0200

    fixed crash when using login endpoint

--- src/main/java/com/norofff/team1/footballapi/service/User_Service.java
+++ src/main/java/com/norofff/team1/footballapi/service/User_Service.java
@@ -9,7 +9,6 @@ import java.util.Optional;
 public interface User_Service {
     List<Users> findAll();
     Optional<Users> findByUsername(String username);
-    Users login(String username);
     Users getOne(int id);
     Users create(Users users);
     Users encodePassword(Users users);
